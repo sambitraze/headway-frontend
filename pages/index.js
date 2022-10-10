0ccc2ab2896296bd908d03ae66eee10ae1d445cf
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-10 14:46:22 +0530

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import IndexEvent from '../components/indexEvent';
+import IndexHero from "../components/indexHero";
+import IndexNav from '../components/indexnav';
+export default function Home() {
+  return (
+    <>
+      <IndexNav />
+      <IndexHero />
+      <IndexEvent />
+    </>
+  )
+}
