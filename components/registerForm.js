4e6d6c232536a89ed007616b4f4e66fe523e86f3
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-10 17:14:57 +0530

    Update registerForm.js

--- components/registerForm.js
+++ components/registerForm.js
@@ -5,7 +5,9 @@ import {
     FormLabel,
     Input,
     InputGroup,
+    Switch,
     HStack,
+    Select,
     InputRightElement,
     Stack,
     Button,
@@ -13,87 +15,119 @@ import {
     Text,
     useColorModeValue,
     Link,
-  } from '@chakra-ui/react';
-  import { useState } from 'react';
-  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
-  
-  export default function RegisterForm() {
-    const [showPassword, setShowPassword] = useState(false);
-  
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
+export default function RegisterForm() {
+    const [showWpPh, setShowWpPh] = useState(false);
+
     return (
-      <Flex
-        minH={'10vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'} textAlign={'center'}>
-              Register
-            </Heading>
-            <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool events ✌️
-            </Text>
-          </Stack>
-          <Box
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={8}>
-            <Stack spacing={4}>
-              <HStack>
-                <Box>
-                  <FormControl id="firstName" isRequired>
-                    <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
+        <Flex
+            minH={'10vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'4xl'} textAlign={'center'}>
+                        Register
+                    </Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        to enjoy all of our cool events ✌️
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <HStack>
+                            <Box>
+                                <FormControl id="firstName" isRequired>
+                                    <FormLabel>First Name</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                            <Box>
+                                <FormControl id="lastName">
+                                    <FormLabel>Last Name</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                        </HStack>
+                        <FormControl id="email" isRequired>
+                            <FormLabel>Email address (Personal)</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <HStack>
+                            <Box>
+                                <FormControl id="roll" isRequired>
+                                    <FormLabel>Roll</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                            <Box>
+                                <FormControl id="year">
+                                    <FormLabel>Year</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                        </HStack>
+                        <FormControl id="branch" isRequired>
+                            <FormLabel>Branch</FormLabel>
+                            <Select placeholder='Select option'>
+                                <option value='option1'>Option 1</option>
+                                <option value='option2'>Option 2</option>
+                                <option value='option3'>Option 3</option>
+                            </Select>
+                        </FormControl>
+
+                        {!showWpPh ? <Box>
+                            <FormControl id="phone" isRequired>
+                                <FormLabel>Phone Number</FormLabel>
+                                <Input type="text" />
+                            </FormControl>
+                        </Box> : <HStack>
+                            <Box>
+                                <FormControl id="phone1" isRequired>
+                                    <FormLabel>Phone Number</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box> <Box>
+                                <FormControl id="phone2" isRequired>
+                                    <FormLabel>Whatsapp Number</FormLabel>
+                                    <Input type="text" />
+                                </FormControl>
+                            </Box>
+                        </HStack>}
+
+                        <FormControl display='flex' alignItems='center'>
+                            <FormLabel htmlFor='email-alerts' mb='0'>
+                                Phone number is same as Whatsapp number?
+                            </FormLabel>
+                            <Switch id='email-alerts' isChecked={showWpPh} onChange={(e) => {
+                                setShowWpPh(!showWpPh);
+                            }} />
+                        </FormControl>
+                        <Stack spacing={10} pt={2}>
+                            <Button
+                                loadingText="Submitting"
+                                size="lg"
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}>
+                                Register
+                            </Button>
+                        </Stack>
+                        <Stack pt={6}>
+                        </Stack>
+                    </Stack>
                 </Box>
-                <Box>
-                  <FormControl id="lastName">
-                    <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
-                  </FormControl>
-                </Box>
-              </HStack>
-              <FormControl id="email" isRequired>
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password" isRequired>
-                <FormLabel>Roll</FormLabel>
-                <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
-                    <Button
-                      variant={'ghost'}
-                      onClick={() =>
-                        setShowPassword((showPassword) => !showPassword)
-                      }>
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                    </Button>
-                  </InputRightElement>
-                </InputGroup>
-              </FormControl>
-              <Stack spacing={10} pt={2}>
-                <Button
-                  loadingText="Submitting"
-                  size="lg"
-                  bg={'blue.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'blue.500',
-                  }}>
-                  Sign up
-                </Button>
-              </Stack>
-              <Stack pt={6}>
-                <Text align={'center'}>
-                  Already a user? <Link color={'blue.400'}>Login</Link>
-                </Text>
-              </Stack>
             </Stack>
-          </Box>
-        </Stack>
-      </Flex>
+        </Flex>
     );
-  }
+}
