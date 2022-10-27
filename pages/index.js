253ea310133433c79e1ebd510a7318aaec50d05a
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-27 19:39:51 +0530

    up

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import IndexEvent from '../components/indexEvent';
 import IndexHero from "../components/indexHero";
 import IndexNav from '../components/indexnav';
-import IndexFooter from '../components/indexFooter';
+import IndexFooter from '../components/IndexFooter';
 import IndexStats from '../components/IndexStat';
 import IndexPricing from '../components/indexPricing';
 export default function Home() {
