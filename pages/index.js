9b1401031bf68233ccc8b5ac623cfc68a49179be
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-27 19:47:22 +0530

    up

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import IndexEvent from '../components/indexEvent';
 import IndexHero from "../components/indexHero";
 import IndexNav from '../components/indexnav';
-import IndexFooter from '../components/IndexFooter';
+// import IndexFooter from '../components/IndexFooter';
 import IndexStats from '../components/IndexStat';
 import IndexPricing from '../components/indexPricing';
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
       <IndexEvent />
       <IndexStats />
       <IndexPricing />
-      <IndexFooter />      
+      {/* <IndexFooter />       */}
     </div>
   )
 }
--- pages/register.js
+++ pages/register.js
@@ -1,13 +1,13 @@
 import IndexNav from "../components/indexnav";
 import RegisterForm from "../components/registerForm";
-import IndexFooter from '../components/IndexFooter';
+// import IndexFooter from '../components/IndexFooter';
 
 export default function Register() {
     return (
         <div>
             <IndexNav />
             <RegisterForm />
-            <IndexFooter />
+            {/* <IndexFooter /> */}
         </div>
     )
 }
