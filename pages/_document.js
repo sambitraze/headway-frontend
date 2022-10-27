a92031137d9c060c0a206d2df73385940afb76b5
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-27 19:33:48 +0530

    final push ui

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <script
-          type="application/javascript"
-          src={`https://${process.env.PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}
-          crossOrigin="true"
-        ></script>
       </Head>
       <body>
         <Main />
