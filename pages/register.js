a30e92a04f6e04c2f99981575e653a3bf1c31ab5
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-27 19:42:22 +0530

    up

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import IndexNav from "../components/indexnav";
 import RegisterForm from "../components/registerForm";
-import IndexFooter from '../components/IndexFooter';
+import IndexFooter from '../components/IndexFooter.js';
 
 export default function Register() {
     return (
