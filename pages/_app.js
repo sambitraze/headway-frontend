6c260ed4776944930f9c594ede64cb4002a9b1db
SAMBIT MAJHI <majhisambit2@gmail.com>
2022-10-27 19:50:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import IndexFooter from '../components/IndexFooter';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <IndexFooter />
     </ChakraProvider>
   )
 }
